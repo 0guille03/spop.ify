0a5dddbae56a1890801d9187f03571a6b066d6f2
100428973 <100428973@alumnos.uc3m.es>
2022-10-26 18:55:19 +0200

    inicio cookie

--- script/sign-in-page.js
+++ script/sign-in-page.js
@@ -1,7 +1,7 @@
 
 function myFunction() {
     // Get the value of the input field with id="numb"
-    let x = document.getElementById("password").value;
+    let x = document.getElementById("name").value;
     // If x is Not a Number or less than one or greater than 10
     let text;
     if (isNaN(x) || x < 1 || x > 10) {
@@ -9,5 +9,49 @@ function myFunction() {
     } else {
         text = "Input OK";
     }
-    document.getElementById("demo").innerHTML = text;
+    document.getElementById("demo").innerHTML = x;   
+}
+
+
+function createCookie() {
+    // Get the value of the input field with id="numb"
+    let x = document.getElementById("name").value;
+    setCookie("cookie_name", x, 30)
+
+}
+
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+  
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+}
+  
+function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+       user = prompt("Please enter your name:","");
+       if (user != "" && user != null) {
+         setCookie("username", user, 30);
+       }
+    }
 }
