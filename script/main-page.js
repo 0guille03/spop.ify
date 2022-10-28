3749d2a6f1a4c76a6fc3047897b7545a653f1694
laumardi <100428953@alumnos.uc3m.es>
2022-10-28 11:28:41 +0200

    Moved javascript references to end of file and dropdown works

--- script/main-page.js
+++ script/main-page.js
@@ -4,7 +4,7 @@ function myFunction() {
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-if (!event.target.matches('.dropbtn')) {
+if (!event.target.matches('.profile_pic')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
