209e9c097478bc46260cd367ce87e831f52dab29
100428973 <100428973@alumnos.uc3m.es>
2022-10-30 00:55:47 +0200

    try validacion

--- script/sign-in-page.js
+++ script/sign-in-page.js
@@ -1,28 +1,74 @@
-function checkPassword () {
+function checkReg (string, re) {
     // Get the value of the input field with id="numb"
-    let x = document.getElementById("password").value;
-    let text = "holi";
-
-    var re = new RegExp("^([a-z0-9]{5,})$");
-    if (re.test(x)) {
-        text = "Input not valid";
-    } else {
-        text = "Input OK";
-        return false;
+    var re = new RegExp(re);
+    if (re.test(string)) {
+        return true;
     }
-    document.getElementById("demo").innerHTML = x;
-    return false;  
+    return false;
 }
 
+function checkForm() {
+    let output = "";
+    // Check name 
+    let name = document.getElementById("name").value;
+    if (!(checkReg(name, "^([A-Za-z][a-z]+)$"))){
+      output = "Incorrect name format";
+      return output
+    }
+    // Check surname 
+    let surname = document.getElementById("surname").value;
+    if (!(checkReg(surname, "^([A-Za-z][a-z]+)$"))){
+      output = "Incorrect surname format";
+      return output
+    }
+    // Check email 
+    let email = document.getElementById("email").value;
+    if (!(checkReg(email, "^([A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]{1,3})$"))){
+      output = "Incorrect email format";
+      return output
+    }
+    /*// Check dob 
+    let dob = document.getElementById("dob").value;
+    let today = new Date().toISOString().slice(0, 10)
+    if (!(today.getTime() > dob.getTime())){
+      output = "Incorrect date of birth";
+      return output
+    }*/
+    // Check username 
+    let username = document.getElementById("username").value;
+    if (!(checkReg(username, "^([A-Za-z0-9]+$"))){
+      output = "Incorrect username format";
+      return output
+    }/*
+    // Check password 
+    let password = document.getElementById("password").value;
+    if (!(checkReg(password, "^([A-Za-z0-9]{1,8}$"))){
+      output = "Incorrect password format";
+      return output
+    }*/
+    return output
+  }    
 
-function createCookie() {
-    if (document.checkPassword() == true){
+
+function validateForm() {
+    /*if (document.checkPassword() == true){
       // Get the value of the input field with id="numb"
       let x = document.getElementById("name").value;
       setCookie("cookie_name", x, 30)
+    }*/
+    //let y = document.getElementById("password").value;
+    //document.getElementById("demo").innerHTML = checkPassword(y);
+    /*if (checkPassword(y)){
+      let y= 'no holi';
+      document.getElementById("demo").innerHTML = y;
+    }*/
+    //let x = document.getElementById("password").value;
+    let output = checkForm();
+    if (output == ""){
+      alert("Correct form");
     }
+    document.getElementById("demo").innerHTML = output;
 }
-
 
 function setCookie(cname,cvalue,exdays) {
     const d = new Date();
