8892118d97b39b0f29f85d8b14180e203376cad4
100428973 <100428973@alumnos.uc3m.es>
2022-10-30 12:18:02 +0100

    cookie created

--- script/sign-in-page.js
+++ script/sign-in-page.js
@@ -8,71 +8,61 @@ function checkReg (string, re) {
 }
 
 function checkForm() {
-    let output = "";
-    // Check name 
-    let name = document.getElementById("name").value;
-    if (!(checkReg(name, "^([A-Za-z][a-z]+)$"))){
-      output = "Incorrect name format";
+      let output = "";
+      // Check name 
+      let name = document.getElementById("name").value;
+      if (!(checkReg(name, "^([A-Za-z][a-z]+)$"))){
+          output = "Incorrect name format";
+          return output;
+      }
+      // Check surname 
+      let surname = document.getElementById("surname").value;
+      if (!(checkReg(surname, "^([A-Za-z][a-z]+)$"))){
+          output = "Incorrect surname format";
+          return output;
+      }
+      // Check email 
+      let email = document.getElementById("email").value;
+      if (!(checkReg(email, "^([A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]{1,3})$"))){
+          output = "Incorrect email format";
+          return output;
+      }
+      // Check username 
+      let username = document.getElementById("username").value;
+      if (!(checkReg(username, "^([A-Za-z0-9]+)$"))){
+          output = "Incorrect username format";
+          return output;
+      }
+      // Check username 
+      let password = document.getElementById("password").value;
+      if (!(checkReg(password, "^([A-Za-z0-9]{1,8})$"))){
+          output = "Incorrect password format";
+          return output;
+      }
       return output;
-    }
-    // Check surname 
-    let surname = document.getElementById("surname").value;
-    if (!(checkReg(surname, "^([A-Za-z][a-z]+)$"))){
-      output = "Incorrect surname format";
-      return output;
-    }
-    // Check email 
-    let email = document.getElementById("email").value;
-    if (!(checkReg(email, "^([A-Za-z0-9]+@[A-Za-z0-9]+\.[A-Za-z0-9]{1,3})$"))){
-      output = "Incorrect email format";
-      return output;
-    }
-    // Check username 
-    let username = document.getElementById("username").value;
-    if (!(checkReg(username, "^([A-Za-z0-9]+)$"))){
-      output = "Incorrect username format";
-      return output;
-    }
-    // Check username 
-    let password = document.getElementById("password").value;
-    if (!(checkReg(password, "^([A-Za-z0-9]{1,8})$"))){
-      output = "Incorrect password format";
-      return output;
-    }
-    return output;
-
-    /*// Check dob 
-    let dob = document.getElementById("dob").value;
-    let today = new Date().toISOString().slice(0, 10)
-    if (!(today.getTime() > dob.getTime())){
-      output = "Incorrect date of birth";
-      return output
-    }*/
-    // Check username 
-    /*let username = document.getElementById("username").value;
-    if (!(checkReg(username, "^([A-Za-z0-9]+$"))){
-      output = "Incorrect username format";
-      return output;
-    }*/
   }    
 
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
 
 function validateForm() {
-    /*if (document.checkPassword() == true){
-      // Get the value of the input field with id="numb"
-      let x = document.getElementById("name").value;
-      setCookie("cookie_name", x, 30)
-    }*/
-    //let y = document.getElementById("password").value;
-    //document.getElementById("demo").innerHTML = checkPassword(y);
-    /*if (checkPassword(y)){
-      let y= 'no holi';
-      document.getElementById("demo").innerHTML = y;
-    }*/
-    //let x = document.getElementById("password").value;
     let output = checkForm();
     if (output == ""){
-      alert("Correct form");
+        alert("Correct form");
+        let username = document.getElementById("username").value;
+        let name = document.getElementById("name").value;
+        let surname = document.getElementById("surname").value;
+        let email = document.getElementById("email").value;
+        let dob = document.getElementById("dob").value;
+        let pict = document.getElementById("pict").value;
+        let password = document.getElementById("password").value;
+        var list = [password, name, surname, email, dob, pict];
+        setCookie(username, JSON.stringify(list), 30);
+        output = "cookie created";
     }
     document.getElementById("demo").innerHTML = output;
 }
