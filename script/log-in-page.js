7b6722e2105253ee01ae4dde2ceb9858af89d8c4
100428973 <100428973@alumnos.uc3m.es>
2022-10-31 11:19:28 +0100

    try delete and log in

--- script/log-in-page.js
+++ script/log-in-page.js
@@ -1,3 +1,16 @@
+/*function delCookie(cname) { 
+    var date = new Date();
+    date.setTime(date.getTime()+(-1*24*60*60*1000));
+    document.cookie = cname + "=" + cookieValue + "; expires=" + date.toGMTString();
+}*/
+
+function setCookie(cname,cvalue,exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
@@ -12,4 +25,32 @@ function getCookie(cname) {
       }
     }
     return "";
+}
+
+function validateLogIn() {
+    let output= "";
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    let cookie_text = getCookie(username);
+    if (cookie_text == ""){
+        setCookie("username", username, 30);
+        output = "No user with that username";
+    }else{
+        var user_cookie = JSON.parse(cookie_text);
+        if (user_cookie[0] == password){
+            setCookie("username", username, 30);
+            window.location.href = "main-page-after-login.html";
+        }
+        output = "Pasword Incorrect";
+    }
+    document.getElementById("demo").innerHTML = output;
+
+}
+
+function checkUser() {
+    let cookie_text = getCookie("username");
+    if (cookie_text != ""){
+        window.location.href = "main-page-after-login.html";
+    }
 }
