e51f74846b55dcbcf0f36bdfd74676b95f93e2a4
100428973 <100428973@alumnos.uc3m.es>
2022-10-31 12:58:54 +0100

    main funka

--- script/main-page-before-login.js
+++ script/main-page-before-login.js
@@ -26,7 +26,6 @@ function myCountdown(countDownDate, id){
         
     // If the count down is over, write some text 
     if (distance < 0) {
-        clearInterval(x);
         document.getElementById(id).innerHTML = "EXPIRED";
     }
 }
