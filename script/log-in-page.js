7ae5aecdadd0ec879e8ec4175a3897959f0eae35
laumardi <100428953@alumnos.uc3m.es>
2022-11-04 17:35:08 +0100

    Added comments to javascript and some refactoring

--- script/log-in-page.js
+++ script/log-in-page.js
@@ -1,32 +1,4 @@
-/*function delCookie(cname) { 
-    var date = new Date();
-    date.setTime(date.getTime()+(-1*24*60*60*1000));
-    document.cookie = cname + "=" + cookieValue + "; expires=" + date.toGMTString();
-}*/
-
-function setCookie(cname,cvalue,exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    let expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
-function getCookie(cname) {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-}
-
+// Function to check whether the username and password is valid
 function validateLogIn() {
     let output= "";
     let username = document.getElementById("username").value;
@@ -46,11 +18,4 @@ function validateLogIn() {
     }
     document.getElementById("demo").innerHTML = output;
 
-}
-
-function checkUser() {
-    let cookie_text = getCookie("username");
-    if (cookie_text != ""){
-        window.location.href = "main-page-after-login.html";
-    }
 }
