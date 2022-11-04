6daa9ef54ac90944f5369899e77aa3b2fa75c782
100428973 <100428973@alumnos.uc3m.es>
2022-11-04 18:27:03 +0100

    crear playlist

--- script/profile.js
+++ script/profile.js
@@ -11,4 +11,15 @@ function loadUserInfo(){
       image.src=user_cookie[5];
       prof_image.src=user_cookie[5];
     }
+    genPlaylist(user_cookie[6]);
+}
+
+function genPlaylist(playlists){
+
+  for (let i = 0; i < playlists.length; i++) {
+      var target = document.getElementById("playlists");
+
+      target.innerHTML += "<div class='grid-song'><div class='grid2'><div><img class='cover' src=" + playlists[i][1] + " width='150' height='150' onclick=\"changeSong()\"></div><div class='song-name'><div><b>" + playlists[i][0] + "</b> <br></div></div></div></div>";
+  }
+
 }
